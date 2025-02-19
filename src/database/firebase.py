c8caa7e111294cde3f634ff5098d8c42559295e2
odetv <abdiutama02@gmail.com>
2025-02-19 21:53:25 +0800

    chore: adding firebase config

---
+++ src/database/firebase.py
@@ -0,0 +1,28 @@
+import os
+import firebase_admin
+from dotenv import load_dotenv
+from firebase_admin import credentials, firestore
+
+
+load_dotenv()
+
+
+def init_firebase():
+    cred = credentials.Certificate({
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+    })
+    try:
+        firebase_admin.get_app()
+    except ValueError:
+        firebase_admin.initialize_app(cred)
+    return firestore.client()
